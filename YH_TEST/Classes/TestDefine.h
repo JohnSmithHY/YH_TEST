c1dcae6f9ac077f07b291d00357ce6885674df5b
长白山 <wywlchangbaishan@163.com>
2018-04-08 00:52:55 +0800

    新增头文件

---
+++ YH_TEST/Classes/TestDefine.h
@@ -0,0 +1,13 @@
+//
+//  TestDefine.h
+//  TestFmdb
+//
+//  Created by John on 2018/4/8.
+//  Copyright © 2018年 John. All rights reserved.
+//
+
+#ifndef TestDefine_h
+#define TestDefine_h
+#import "TestObj.h"
+
+#endif /* TestDefine_h */
